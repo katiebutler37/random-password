a6084986b8096779b1c10d7221226b7d9e98aba4
Katie <katiebutler37@gmail.com>
2022-05-17 22:21:00 -0400

    prompt sequence upon button click initiated

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,21 @@
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
 // Assignment code here
 
+
+ generateBtn.onclick = function() {
+    var promptLength = window.prompt("Please select a password length between 8 - 128 characters."); 
+      console.log(promptLength);
+      
+
+
+  }
+  
+
+
+//will eventually be the function to combine responses into random password calculation to be inputted
+//var generatePassword = function()
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
