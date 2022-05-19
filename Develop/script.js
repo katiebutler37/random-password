8e98f4a27e06bbac8e05fa93c8bfee18c8106f1e
Katie <katiebutler37@gmail.com>
2022-05-19 01:04:09 -0400

    created concat array to pull characters from

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,31 @@
-var numberChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialChars = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-
 var passwordLength; 
 var hasNumbers;
 var hasSpecialCharacters;
 var hasUppercase;
 var hasLowercase;
+
+var numberChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChars = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+var selectedChars;
+if (hasLowercase) {
+  selectedChars = lowerChars.concat(selectedChars);
+}
+if (hasUppercase) {
+  selectedChars = upperChars.concat(selectedChars);
+}
+if (hasNumbers) {
+  selectedChars = numberChars.concat(selectedChars);
+}
+if (hasSpecialCharacters) {
+  selectedChars = specialChars.concat(selectedChars);
+}
+
+
+
+
 
   var criteriaLength = function() {
   //ask user to input numeric value for password length
@@ -29,7 +47,7 @@ var hasLowercase;
 
         return(hasUppercase);
 
-   }
+   };
 
  //ask user to decide if to include lowercase letters 
   var criteriaLowercase = function() {
@@ -37,7 +55,7 @@ var hasLowercase;
      console.log(hasLowercase);
 
      return(hasLowercase);
-}   
+};   
 
   var criteriaSpecialCharacters = function() {
    // ask user to decide if to include special characters  
@@ -45,7 +63,7 @@ var hasLowercase;
       console.log(hasSpecialCharacters);
 
       return(hasSpecialCharacters);
-    }
+    };
 
   var criteriaNumbers = function() {
     //ask user to decide if to include Numbers
@@ -53,11 +71,11 @@ var hasLowercase;
       console.log(hasNumbers);
 
       return(hasNumbers);
-  }  
+  }; 
 
 var makePassword = function() {
   
-}  
+};  
    
 //will eventually be the function to combine responses into random password calculation to be inputted
 var generatePassword = function() {
