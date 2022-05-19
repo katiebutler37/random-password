2d1a4efc01770f03fd4385564ac806d64a051d64
Katie <katiebutler37@gmail.com>
2022-05-18 22:13:50 -0400

    changed window prompts to confirms to structure variables into has___ format for true/false values to be stored and inputted into generatePassword function, all variables needed for the function are stored globally

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,11 @@
 var getLength; 
 var hasNumbers;
+var hasSpecialCharacters;
+var hasNumbers;
+var hasUppercase;
+var hasLowercase;
 
-var criteriaLength = function() {
+  var criteriaLength = function() {
   //ask user to input numeric value for password length
    getLength = window.prompt("Please select a password length between 8 - 128 characters."); 
      if (getLength >= 8 && getLength <= 128) {
@@ -14,54 +18,49 @@ var criteriaLength = function() {
      return(getLength); 
    } 
 
- //ask user to input 1,2 or 3, to decide on the cases used in the password  
-  var criteriaCase = function() {
-     var getCase = window.prompt(" Would you like your password to contain UPPERCASE characters, LOWERCASE characters or BOTH? Please enter 1 for UPPERCASE, 2 for LOWERCASE, or 3 for BOTH.");
-    // convert answer from prompt to an actual number
-    getCase = parseInt(getCase);
-    // use switch case to carry out action
-    switch (getCase) {
-      case 1:
-        console.log("uppercase");
-        return("uppercase");
-      case 2:
-        console.log("lowercase");
-        return("lowercase");
-      case 3:
-        console.log("both cases");
-        return("both cases")
-      default:
-        window.alert("Please select a valid response.")
-        criteriaCase();
-    }
+ //ask user to decide if to include uppercase letters 
+  var criteriaUppercase = function() {
+     hasUppercase = window.confirm(" Would you like your password to contain UPPERCASE characters?");
+        console.log(hasUppercase);
+
+        return(hasUppercase);
+
    }
 
-   var criteriaCharacterType = function() {
-     var getCharacterType = window.prompt("Would you like your password to contain NUMBERS, SPECIAL CHARACTERS or BOTH? Please enter 1 for NUMBERS, 2 for SPECIAL CHARACTERS, or 3 for BOTH.");
-     // convert answer from prompt to an actual number
-     getCharacterType = parseInt(getCharacterType);
-     switch (getCharacterType) {
-      case 1:
-        console.log("numbers");
-        return("numbers");
-      case 2:
-        console.log("special characters");
-        return("special characters");
-      case 3:
-        console.log("both types");
-        return("both types")
-      default:
-        window.alert("Please select a valid response.")
-        criteriaCharacterType();
+ //ask user to decide if to include lowercase letters 
+  var criteriaLowercase = function() {
+    hasLowercase = window.confirm(" Would you like your password to contain lowercase characters?");
+     console.log(hasLowercase);
+
+     return(hasLowercase);
+}   
+
+  var criteriaSpecialCharacters = function() {
+   // ask user to decide if to include special characters  
+     hasSpecialCharacters = window.confirm("Would you like your password to include special characters?");
+      console.log(hasSpecialCharacters);
+
+      return(hasSpecialCharacters);
     }
-   }
+
+  var criteriaNumbers = function() {
+    //ask user to decide if to include Numbers
+    hasNumbers = window.confirm("Would you like you password to include numbers?");
+      console.log(hasNumbers);
+
+      return(hasNumbers);
+  }  
+   
 
 //will eventually be the function to combine responses into random password calculation to be inputted
 var generatePassword = function() {
     criteriaLength();
-    criteriaCase();
-    criteriaCharacterType();
-    return 
+    criteriaUppercase();
+    criteriaLowercase();
+    criteriaSpecialCharacters();
+    criteriaNumbers();
+
+    return //makePassword()
   }  
     
 // Get references to the #generate element
